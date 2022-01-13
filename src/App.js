5577351f265c56577b11fdde69e2eb81a799cf94
Cafrey3 <tokar9980@gmail.com>
2022-01-14 00:31:36 +0200

    cw1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import React from 'react';
+
+import './App.css';
 import SpaceX from "./components/spaceX/SpaceX";
+import './components/spaceX/SpaceX.css';
 
 const App = () => {
     return (
-        <div>
+        <>
             <SpaceX/>
-        </div>
+        </>
     );
 };
 
--- src/components/spaceX/SpaceX.js
+++ src/components/spaceX/SpaceX.js
@@ -5,18 +5,18 @@ const SpaceX = () => {
     const [spaceX, setSpaceX] = useState([]);
 
     useEffect(() => {
-        fetch('')
+        fetch('https://api.spacexdata.com/v3/launches/')
             .then(value => value.json())
             .then((value) => {
-                let filter = value.filter(value => value.launch_year !== 2020);
+                let filter = value.filter(value => value.launch_year !== '2020');
                 setSpaceX(filter);
             });
     }, []);
 
     return (
         <div>
-            {spaceX.map(value => <div key={value.flight_number}>
-                {value.mission_name} {value.launch_year}
+            {spaceX.map(value => <div className={'space'} key={value.flight_number}>
+                {value.mission_name} {value.launch_year} <img src={value.links.mission_patch_small} alt="mission_patch_small"/>
             </div>)}
         </div>
     );
