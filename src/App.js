15c389322a10091d881a89e6a2f1e555be20a57e
Cafrey3 <tokar9980@gmail.com>
2022-01-28 00:25:34 +0200

    hw2

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import './App.css';
 import Users from "./components/Users/Users";
 import UserDetails from "./components/UserDetails/UserDetails";
 import Posts from "./components/Posts/Posts";
-import {} from './components/User/User.css';
-import {} from './components/Users/Users.css'
-import {} from './components/UserDetails/UserDetails.css'
-import {} from './components/Post/Post.css'
 import {postService} from "./components/services/post.service";
 
 const App = () => {
